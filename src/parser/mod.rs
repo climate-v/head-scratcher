8c57c660520bdb17f36c1d6c3d2386e5ccdb3835
ucyo <cayoglu@me.com>
2021-06-12 17:59:14 +0200

    Add basic skeleton for header format

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -3,8 +3,23 @@
 //! # Parser
 //! Main parsing module
 use crate::error::HeadScratcherError as HSE;
+use components::{NetCDFDimension, NetCDFType, NetCDFVariable, NetCDFVersion};
 use nom::IResult;
+use std::collections::HashMap;
 
 pub mod components;
 
 pub type HSEResult<I, O> = IResult<I, O, HSE<I>>;
+pub type AttributeHM = HashMap<String, String>;
+pub type DimensionHM = HashMap<u32, NetCDFDimension>;
+pub type VariableHM = HashMap<String, NetCDFVariable>;
+
+/// NetCDF file format
+#[derive(Debug)]
+pub struct NetCDFHeader {
+    version: NetCDFVersion,
+    kind: NetCDFType,
+    attrs: Option<AttributeHM>,
+    dims: Option<DimensionHM>,
+    vars: Option<VariableHM>,
+}
