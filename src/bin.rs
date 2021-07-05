92e843a400e5bb631346fc223c44d0c60dd3308d
ucyo <cayoglu@me.com>
2021-07-05 23:27:20 +0200

    Add headscratcher binary with placeholder

---
+++ src/bin.rs
@@ -0,0 +1,8 @@
+
+use netcdf_head_scratcher::parser::header;
+
+fn main() {
+    let i = include_bytes!("../assets/sresa1b_ncar_ccsm3-example.nc");
+    let (_, h) = header(i).unwrap();
+    println!("{:#?}", h.vars)
+}
