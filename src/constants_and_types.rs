5f71e510229bc9652acafe51cd52c70bc5f95315
ucyo <cayoglu@me.com>
2021-07-06 16:04:27 +0200

    Add default fill values

--- src/constants_and_types.rs
+++ src/constants_and_types.rs
@@ -17,5 +17,12 @@ pub const NC_INT:       u32 = 0x00_00_00_04;
 pub const NC_FLOAT:     u32 = 0x00_00_00_05;
 pub const NC_DOUBLE:    u32 = 0x00_00_00_06;
 
+pub const FILL_BYTE:    u8 = 0x81;
+pub const FILL_CHAR:    u8 = 0x00;
+pub const FILL_SHORT:  u16 = 0x80_01;
+pub const FILL_INT:    u32 = 0x80_00_00_01;
+pub const FILL_FLOAT:  u32 = 0x7C_F0_00_00;
+pub const FILL_DOUBLE: u64 = 0x47_9E_00_00_00_00;
+
 // Types
 pub type NON_NEG = u32;
