538c6abb3158612a086e9afda09ef11b56ec1cdb
ucyo <cayoglu@me.com>
2021-07-08 17:13:26 +0200

    Add update function to buffer data at starting coordinate

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ pub mod constants_and_types;
 pub mod error;
 pub mod parser;
 
-// https://cluster.earlham.edu/bccd-ng/testing/mobeen/GALAXSEEHPC/netcdf-4.1.3/man4/netcdf.html#Computing-Offsets
-fn product_vector(vecs: &[usize], record: bool) -> Vec<usize> {
+fn product_vector(vecs: &[usize], _record: bool) -> Vec<usize> {
+    // https://cluster.earlham.edu/bccd-ng/testing/mobeen/GALAXSEEHPC/netcdf-4.1.3/man4/netcdf.html#Computing-Offsets
     let mut prod = 1usize;
     let mut result: Vec<usize> = Vec::new();
     for v in vecs.iter().rev() {
@@ -17,35 +17,26 @@ fn product_vector(vecs: &[usize], record: bool) -> Vec<usize> {
     result
 }
 
-fn read_first_f32<S: Seek + Read>(
+pub fn update_buffer<F: Seek + Read>(
     header: NetCDFHeader,
     var: String,
-    time: Option<usize>,
-    lev: Option<usize>,
-    file: &mut S,
-) -> f32 {
-    let vars = header.vars.unwrap();
-    let pos = vars[&var].begin;
-    file.seek(SeekFrom::Start(pos)).unwrap();
-
-    let mut contents = vec![0u8; 4];
-    file.read_exact(&mut contents).unwrap();
-    let (_, val) = parser::components::float(&contents).unwrap();
-    val
-}
-
-fn update_buffer<F: Seek + Read>(
-    header: NetCDFHeader,
-    var: String,
-    time: Option<usize>,
-    lev: Option<usize>,
+    start: Vec<usize>,
     file: &mut F,
     buffer: &mut [u8],
 ) -> Result<(), std::io::Error> {
-    let vars = header.vars.unwrap();
-    let seek_pos = vars[&var].begin;
-    let time_idx = time.unwrap_or(1);
-    let lev_idx = lev.unwrap_or(1);
+    let seek_pos = match header.vars {
+        Some(vars) => {
+            let v = vars.get(&var).unwrap();
+            assert!(v.dims.len() == start.len(), "Lengths are different");
+            let offset: usize = start
+                .iter()
+                .zip(header.seeks.unwrap().get(&var).unwrap())
+                .map(|(a, b)| a * b)
+                .sum();
+            offset as u64 * v.nc_type.extsize() as u64 + v.begin
+        }
+        None => panic!("Could not find variable '{:?}'", var),
+    };
     file.seek(SeekFrom::Start(seek_pos))?;
     file.read_exact(buffer)
 }
@@ -54,25 +45,6 @@ fn update_buffer<F: Seek + Read>(
 #[allow(unused_imports)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_read_netcdf() {
-        let nc = include_bytes!("../assets/sresa1b_ncar_ccsm3-example.nc");
-        let (_, header) = parser::header(nc).unwrap();
-        let path = "assets/sresa1b_ncar_ccsm3-example.nc";
-        let mut file = std::fs::File::open(path).unwrap();
-        let val = read_first_f32(header, "tas".to_string(), None, None, &mut file);
-        assert_eq!(val, 215.8935)
-    }
-    //     let var = &file.variable("tas").unwrap();
-    //     let first: f32 = var.value(None).unwrap();
-    //     assert_eq!(first, 215.8935);
-    //     let flevel = var
-    //         .values::<f32>(Some(&[0, 0, 0]), Some(&[1, 1, 5]))
-    //         .unwrap();
-    //     let expected = vec![215.8935, 215.80531, 215.73935, 215.66304, 215.61963];
-    //     assert_eq!(flevel.into_raw_vec(), expected)
-    // }
 
     #[test]
     fn test_read_level_netcdf() {
@@ -84,14 +56,14 @@ mod tests {
         update_buffer(
             header,
             "tas".to_string(),
-            None,
-            None,
+            vec![0, 0, 0],
             &mut file,
             &mut buffer,
         )
         .unwrap();
-
-        assert_eq!(buffer, vec![67, 87, 228, 188])
+        let (_, f) = parser::components::float(&buffer).unwrap();
+        assert_eq!(f, 215.8935);
+        assert_eq!(buffer, vec![67, 87, 228, 188]);
     }
 
     #[test]
